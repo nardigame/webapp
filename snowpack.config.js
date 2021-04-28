fef9af6a5a1e340f7ac566fe63796999d40814ee
bezdonas <bezdonas@gmail.com>
2021-04-28 20:15:46 +0300

    initial commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,15 +1,26 @@
-// Snowpack Configuration File
-// See all supported options: https://www.snowpack.dev/reference/configuration
-
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  root: './src',
   mount: {
-    /* ... */
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
   plugins: [
-    /* ... */
+    [
+      '@snowpack/plugin-typescript',
+      {
+        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
+        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
+      },
+    ],
   ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
   packageOptions: {
     /* ... */
   },
