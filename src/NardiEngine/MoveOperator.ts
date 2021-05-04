8cd1c3dd00d2753473a4e4707c892dc654d952f3
bezdonas <bezdonas@gmail.com>
2021-05-04 21:18:43 +0300

    applyMovementToBoard realised, needs refactor though

--- src/NardiEngine/MoveOperator.ts
+++ src/NardiEngine/MoveOperator.ts
@@ -1,4 +1,5 @@
-import { Move, Side } from './types';
+import { Move, Side, Movement, Board } from './types';
+import cloneDeep from 'lodash.cloneDeep';
 
 export const toggleTurn = (turn: Side): Side =>
   turn === 'white' ? 'black' : 'white';
@@ -7,3 +8,45 @@ export const recordMove = (move: Move, history: Move[]): Move[] => [
   ...history,
   move,
 ];
+
+// TODO: REFACTOR
+export const applyMovementToBoard = (
+  movement: Movement,
+  board: Board,
+): Board => {
+  const newBoard = cloneDeep(board);
+
+  const [from, to] = movement;
+  let sideFrom;
+
+  if (newBoard[from]) {
+    sideFrom = newBoard[from].side;
+    newBoard[from].quantity -= 1;
+
+    if (newBoard[from].quantity === 0) {
+      newBoard[from] = null;
+    }
+  } else {
+    console.error(
+      'applyMovementToBoard attempted illegal move: seems like validation failed',
+    );
+    return { ...board };
+  }
+
+  if (newBoard[to]) {
+    if (newBoard[to].side !== sideFrom) {
+      console.error(
+        'applyMovementToBoard attempted illegal move: seems like validation failed',
+      );
+      return { ...board };
+    }
+    newBoard[to].quantity += 1;
+  } else {
+    newBoard[to] = {
+      quantity: 1,
+      side: sideFrom,
+    };
+  }
+
+  return newBoard;
+};
